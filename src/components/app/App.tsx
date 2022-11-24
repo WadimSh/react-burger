ebee5686de0e7ad75685f5bd81fff11b99ca32be
WadimSh <wadim11@yandex.ru>
2022-11-24 13:34:34 +0300

    feat: сверстан компонент AppHeader

---
+++ src/components/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AppHeader from '../app-header/app-header';
+
+import style from './App.module.css';
+
+function App() {
+  return (
+    <div className={style.App}>
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
