ade371981f7166597da49fb3633bc3e39b3a7413
WadimSh <wadim11@yandex.ru>
2022-11-25 16:56:40 +0300

    fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,23 +1,32 @@
-export const api = {
-  url: "https://norma.nomoreparties.space/api/ingredients",
-  headers: {
-    "Content-Type": "aplication.json",
-  },
-};
+import { INGREDIENTS_URL } from './constants';
 
-const checkResponse = (res) => {
-  if (res.ok) {
-    return res.json();
-  } 
-  return Promise.reject(
-    `Что-то пошло не так: Ошибка ${res.status} - ${res.statusText}`
-  );
-};
+class Api {
+  constructor({ baseUrl }) {
+    this.url = baseUrl;
+  }
 
-export const getIngredients = () => {
-  return fetch(`${api.url}`)
-    .then(checkResponse)
+  _checkResponse = (res) => {
+    if(res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка ${res.status}`);
+  }
+
+  getIngredients() {
+    return fetch(`${this.url}`, {
+      headers: {
+        "Content-Type": "application.json",
+      },
+    })
+    .then(res => this._checkResponse(res))
     .catch((err) => {
       console.log(err);
     });
-};
+  }
+}
+    
+const api = new Api({
+  baseUrl: `${INGREDIENTS_URL}`,
+});
+
+export default api;
