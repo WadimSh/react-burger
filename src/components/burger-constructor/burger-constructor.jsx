a0135893697ac088b3a13de70e2e3a567e7d46d1
WadimSh <wadim11@yandex.ru>
2022-12-04 12:27:32 +0300

    feat: применен хук useReducer при выводе стоимости бургера

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect, useReducer } from 'react';
 //import PropTypes from 'prop-types';
 
 import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
@@ -11,14 +11,48 @@ import { CurrentContext } from '../../contexts/context';
 //import { messagePropTypes } from '../../utils/messagePropTypes';
 import style from './burger-constructor.module.css';
 
+const initialConstructorState = {
+  bun: {},
+  burgerIngredients: [],
+  totalPrice: 0,
+};
+
 function BurgerConstructor() {
   const { data } = useContext(CurrentContext);
   const [close, setClose] = useState(false);
   const clickButton = () => setClose(!close);
 
-  const main = data.filter((item) => item.type !== "bun");
-  const bun = data.find((item) => item.type === "bun");
-  const totalPrice = main.reduce((total, current) => total + current.price, bun.price * 2);
+//--  
+  const reducer = (state, action) => {
+    const bun = data.find((item) => item.type === 'bun');
+    const burgerIngredients = data.filter((item) => item.type !== 'bun')
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 5);
+    const totalPrice = state.burgerIngredients.reduce((total, current) => total + current.price, state.bun.price * 2);
+    switch (action.type) {
+      case 'initiate':
+        return { ...state, burgerIngredients, bun };
+      case 'count':
+        return { ...state, totalPrice };
+      default:
+        throw new Error(`Ошибка - не правильный тип экшина`);
+    }
+  }
+
+  const [
+    constructorState,
+    constructorStateDispatcher,
+  ] = useReducer(reducer, initialConstructorState);
+
+  useEffect(() => {
+    constructorStateDispatcher({ type: 'initiate' });
+    constructorStateDispatcher({ type: 'count' });
+  }, [data]);
+
+  //--
+  //const main = data.filter((item) => item.type !== "bun");
+  //const bun = data.find((item) => item.type === "bun");
+  //const totalPrice = main.reduce((total, current) => total + current.price, bun.price * 2);
 
   return (
     <section className={style.section}>
@@ -31,26 +65,26 @@ function BurgerConstructor() {
         <ConstructorElement
           type="top"
           isLocked={true}
-          text={`${bun.name} (верх)`}
-          price={`${bun.price}`}
-          thumbnail={`${bun.image}`}
+          text={`${constructorState.bun.name} (верх)`}
+          price={`${constructorState.bun.price}`}
+          thumbnail={`${constructorState.bun.image}`}
         />
       </div>
       <ul className={style.stuffingList}>
-        {data.map((item, index) => item.type !== "bun" && <BurgerConstructorIngredient ingredient={item} key={index} />
+        {constructorState.burgerIngredients.map((item, index) => <BurgerConstructorIngredient ingredient={item} key={index} />
         )}
       </ul>
       <div className={style.bun}>
         <ConstructorElement
           type="bottom"
           isLocked={true}
-          text={`${bun.name} (низ)`}
-          price={`${bun.price}`}
-          thumbnail={`${bun.image}`}
+          text={`${constructorState.bun.name} (низ)`}
+          price={`${constructorState.bun.price}`}
+          thumbnail={`${constructorState.bun.image}`}
         />
       </div>
       <TotalPrice
-       totalPrice={totalPrice}
+       totalPrice={constructorState.totalPrice}
        clickButton={clickButton}
       />
     </section>
