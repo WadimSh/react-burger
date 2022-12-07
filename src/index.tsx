18c9dfde828ce0fa453d10b15fb4b559a6ab3d85
WadimSh <wadim11@yandex.ru>
2022-12-07 23:00:52 +0300

    fix: востановлена работа sprint2/step2

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import './index.css';
-import App from './App';
+import App from './components/app/App.jsx';
+import rootReducer from './services/reducers/root-reducer';
 import reportWebVitals from './reportWebVitals';
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [thunk],
+  devTools: process.env.NODE_ENV !== 'production'
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
